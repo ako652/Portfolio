2e1a9c2752e17c24dfeff240b230b2900e67bb19
ako652 <akotabe0213@gmail.com>
2023-04-27 21:51:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import NavBar from "./components/NavBar"
+import Home from "./components/Home"
+import Grid from "./components/Grid"
 
 function App() {
-  return <div className="App">React homework</div>;
+  return <div className="App">
+    <NavBar />
+    <Home />
+    <Grid />
+    </div>;
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+export default function Home(){
+    return <div className='mainContent'>
+        
+        <div className='Aboutme'>
+        
+        <div className='textin'>
+        I am a passionate IT professional who likes everything about IT, I have a Telecoms  degree with a master  degree  in cybersecurity, apart from IT ,I have a great people and presentation skills that makes me unique.I am a natural problem solver and love to help and solve technical problems
+        </div>
+        </div>
+
+        <div className='Profilepic'>
+            image
+        </div>
+        <h1>Portfolio</h1>
+        <div className='design'>
+            
+        <div className='Techstack'>
+        <h2>  design</h2>
+           
+         outside, tasty on the inside.
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim. Ut porttitor leo a diam sollicitudin. Turpis in eu mi bibendum neque egestas congue . Viverra adipiscing at in tellus integer. <br></br>
+         <button>learn more</button>
+            </div>
+
+        </div>
+
+        
+
+    
+    
+    </div>
+}
